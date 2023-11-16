0f6983752d1534cf3c8b94602e6852b65da67cb1
Ferdinand Gendreau <devferdinandg@gmail.com>
2023-11-16 21:36:32 +0100

    affichage des articles

--- src/form/form.js
+++ src/form/form.js
@@ -1,2 +1,54 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
+
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    try {
+      const json = JSON.stringify(article);
+      const response = await fetch("https://restapi.fr/api/articles", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const body = await response.json();
+      console.log(body);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
+
+const formIsValid = (article) => {
+  if (
+    !article.author ||
+    !article.category ||
+    !article.content ||
+    !article.img ||
+    !article.title
+  ) {
+    errors.push("Veuillez remplir tout les champs");
+  } else {
+    console.log("article ajouté!");
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li> ${e} </li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
